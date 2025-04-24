bd5f48862094da4c849e915fb949a1cfa5221c9b
galal2241 <ahmed2241.galal@gmail.com>
2025-04-24 16:58:37 +0200

    implement QuizController  and test endpoints

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\CourseController;
 use App\Http\Controllers\API\LessonController;
+use App\Http\Controllers\API\QuizController;
 
 Route::get('/test', function () {
     return response()->json(['message' => 'API is working']);
@@ -26,6 +27,15 @@ Route::middleware('auth:api')->group(function () {
     });
 
     Route::prefix('courses/{courseId}/lessons')->group(function () {
+
+        Route::prefix('quizzes')->group(function () {
+            Route::get('/', [QuizController::class, 'index']);
+            Route::post('/', [QuizController::class, 'store']);
+            Route::get('/{id}', [QuizController::class, 'show']);
+            Route::put('/{id}', [QuizController::class, 'update']);
+            Route::delete('/{id}', [QuizController::class, 'destroy']);
+        });
+    
         Route::get('/', [LessonController::class, 'index']);
         Route::post('/', [LessonController::class, 'store']);
         Route::get('/{id}', [LessonController::class, 'show']);
