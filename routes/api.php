86352510ea1d8c442d2dadde3f58219ec49ef956
galal2241 <ahmed2241.galal@gmail.com>
2025-04-24 12:57:21 +0200

    successfully verified authorization flow using Postman

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'API is working']);
+});
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
+});
+
